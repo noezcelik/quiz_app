152e61ed6d23d7f80c2e79e5708e07a88b1a9592
Nasuf Özcelik <oezcelik@bm.local>
2024-01-24 10:08:43 +0100

    dh

--- lib/quiz_page.dart
+++ lib/quiz_page.dart
@@ -14,15 +14,25 @@ class _QuizPageState extends State<QuizPage> {
     return Scaffold(
         appBar: customAppBar("Quiz", Colors.cyan[200]!),
         body: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Hier steht die Frage!"),
-            answerCard("1"),
-            answerCard("2"),
-            answerCard("3"),
-            answerCard("4")
-          ],
+            child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Hier steht die Frage!",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              answerCard("1", context),
+              answerCard("2", context),
+              answerCard("3", context),
+              answerCard("4", context)
+            ],
+          ),
         )));
   }
 }
